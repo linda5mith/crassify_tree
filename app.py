b96357b70c2e983330c48b7e4169a6924ff526a1
linda5mith <linda.jsm@outlook.com>
2023-03-06 22:34:07 +0000

    adding index.html for netlify

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-# >>> import plotly.express as px
-# >>> fig = px.box(range(10))
-# >>> fig.write_html('test.html')
+p = open("empress.html")
+components.html(p.read())
 
-#st.header("test html import")
-
-HtmlFile = open("empress.html", 'r', encoding='utf-8')
-source_code = HtmlFile.read() 
-print(source_code)
-components.html(source_code)
+# HtmlFile = open("empress.html", 'r', encoding='utf-8')
+# source_code = HtmlFile.read() 
+# print(source_code)
+# components.html(source_code)
